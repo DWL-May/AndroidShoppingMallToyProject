531be574410c566c36add6b8c704eb85d109704e
dawon94 <ssop6403@gmail.com>
2020-04-25 20:39:07 +0900

    - Add Register Function Server

--- server/Parayo/src/main/kotlin/com/example/parayo/domain/auth/SignupService.kt
+++ server/Parayo/src/main/kotlin/com/example/parayo/domain/auth/SignupService.kt
@@ -3,26 +3,31 @@ package com.example.parayo.domain.auth
 import com.example.parayo.common.ParayoException
 import com.example.parayo.domain.User
 import com.example.parayo.domain.UserRepository
+import org.mindrot.jbcrypt.BCrypt
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class SignupService @Autowired constructor(
-        private val userRepository: UserRepository
+    private val userRepository: UserRepository
 ) {
     fun signup(signupRequest: SignupRequest) {
-
+        validateRequest(signupRequest)
+        checkDuplicates(signupRequest.email)
+        registerUser(signupRequest)
     }
 
     private fun validateRequest(signupRequest: SignupRequest) {
-
+        validateEmail(signupRequest.email)
+        validateName(signupRequest.name)
+        validatePassword(signupRequest.password)
     }
 
     private fun validateEmail(email: String) {
         val isNotValidEmail = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$"
-                .toRegex(RegexOption.IGNORE_CASE)
-                .matches(email)
-                .not()
+            .toRegex(RegexOption.IGNORE_CASE)
+            .matches(email)
+            .not()
         if (isNotValidEmail) {
             throw ParayoException("이메일 형식이 올바르지 않습니다.")
         }
@@ -41,11 +46,14 @@ class SignupService @Autowired constructor(
     }
 
     private fun checkDuplicates(email: String) =
-            userRepository.findByEmail(email)?.let {
-                throw ParayoException("이미 사용 중인 이메일입니다.")
-            }
+        userRepository.findByEmail(email)?.let {
+            throw ParayoException("이미 사용 중인 이메일입니다.")
+        }
 
     private fun registerUser(signupRequest: SignupRequest) =
-            with(signupRequest) {
-            }
+        with(signupRequest) {
+            val hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt())
+            val user = User(email, hashedPassword, name)
+            userRepository.save(user)
+        }
 }
